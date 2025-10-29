9fdc767c6ddfac137956cbe93fc15036bfa5572c
omerM2 <115428099+omerM2@users.noreply.github.com>
2025-10-29 18:28:16 +0200

    Implement random number generation and sorting
    Generates three random integers in specified ranges and prints them in sorted order.

--- Ascend.java
+++ Ascend.java
@@ -1,7 +1,21 @@
 // Generates three integer random numbers in a given range,
 // and prints them in increasing order.
+import java.util.Arrays;
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+
+		int a = (int)(Math.random() * 11 ); // generates a random number in the range of 0 - 10
+		int b = (int)(Math.random() * 11 + 5 ); // generates a random number in the range of 5 - 15
+		int c = (int)(Math.random() * 101 + 10 );// generates a random number in the range of 10 - 110
+
+		int[] myArray = { a , b , c };
+		Arrays.sort(myArray);
+		System.out.println(Arrays.toString(myArray));
+
+
+
+		
 	}
-}
+
+
+	}
