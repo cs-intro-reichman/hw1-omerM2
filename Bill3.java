399ac59ca9c0f0b1ffe99986b66456ac613d3881
omerM2 <115428099+omerM2@users.noreply.github.com>
2025-10-30 12:36:40 +0200

    Add support for three names and total sum calculation

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,14 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
+		String name2 = args[1];
+		String name3 = args[2];
+		String strTotalSum = args[3];
+
+		double doubleTotalSum = Double.parseDouble(strTotalSum);
+		double personalAmount = Math.ceil(doubleTotalSum / 3);
+		System.out.println("Dear "+ name1 + ", " + name2 + ", and " + name3 + ": pay " + personalAmount + " Shekels each.");
+
 	    // Replace this comment with the rest of your code   
 	}
 }
