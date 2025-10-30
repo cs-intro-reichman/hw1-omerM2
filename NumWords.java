bd2b2b2264570469c8fd18382666186638551fee
omerM2 <115428099+omerM2@users.noreply.github.com>
2025-10-30 19:48:53 +0200

    Refactor number printing logic in NumWords.java

--- NumWords.java
+++ NumWords.java
@@ -3,15 +3,14 @@ public class NumWords {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
 		String number = args[0];
-		if (number.length() == 3){
-			System.out.println("" + number.charAt(0) + " hundreds, " + number.charAt(1) + " tens, and " + number.charAt(2) + " ones."  );
-		}
-		if (number.length() == 2){
-			System.out.println("0 hundreds, " + number.charAt(0) + " tens, and " + number.charAt(1) + " ones."  );
-		}
 
-		if (number.length() == 1){
-			System.out.println("0 hundreds, 0 tens, and " + number.charAt(0) + " ones."  );
-		}
+		int intNum = Integer.parseInt(number);
+		int hundreds = intNum / 100 ;
+		int tens = (intNum / 10) % 10;
+		int ones = intNum % 10;
+		
+		System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.");
+
+
 	}
 }
