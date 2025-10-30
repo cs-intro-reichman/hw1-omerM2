6b15a723aea8b86f302d1580ecc08caab249c936
omerM2 <115428099+omerM2@users.noreply.github.com>
2025-10-30 19:35:15 +0200

    Implement number printing in words
    Add functionality to print numbers in hundreds, tens, and units.

--- NumWords.java
+++ NumWords.java
@@ -2,5 +2,16 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
+		String number = args[0];
+		if (number.length() == 3){
+			System.out.println(number.charAt(0) + " hundreds, " + number.charAt(1) + " tens, and " + number.charAt(2) + " ones."  );
+		}
+		if (number.length() == 2){
+			System.out.println("0 hundreds, " + number.charAt(0) + " tens, and " + number.charAt(1) + " ones."  );
+		}
+
+		if (number.length() == 1){
+			System.out.println("0 hundreds, 0 tens, and " + number.charAt(0) + " ones."  );
+		}
 	}
 }
