a32da04e711352f81ab4c38b06c00a3fc517ba2d
omerM2 <115428099+omerM2@users.noreply.github.com>
2025-12-08 05:22:54 +0200

    Change order of names in output message

--- Bill3.java
+++ Bill3.java
@@ -11,7 +11,7 @@ public class Bill3 {
 
 
 
-		System.out.println("Dear "+name1+", "+name2+", and "+name3+": pay "+sum+" Shekels each.");
+		System.out.println("Dear "+name3+", "+name2+", and "+name1+": pay "+sum+" Shekels each.");
 
 	    // Replace this comment with the rest of your code   
 	}
