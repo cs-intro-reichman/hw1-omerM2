6fbb8be722769a950218d13e97ad876d7dda5856
omerM2 <115428099+omerM2@users.noreply.github.com>
2025-12-08 05:12:59 +0200

    Refactor variable names and simplify output

--- Bill3.java
+++ Bill3.java
@@ -2,14 +2,16 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
+		String name1 = args[0];
 		String name2 = args[1];
 		String name3 = args[2];
-		String strTotalSum = args[3];
+		String stringSum = args[3];
+		double sum = Double.parseDouble(stringSum);
+		sum = Math.ceil(sum);
 
-		double doubleTotalSum = Double.parseDouble(strTotalSum);
-		double personalAmount = Math.ceil(doubleTotalSum / 3);
-		System.out.println("Dear "+ name3 + ", " + name2 + ", and " + name1 + ": pay " + personalAmount + " Shekels each.");
+
+
+		System.out.println("Dear "+name1+", "+name2+", and "+name3+": pay "+sum+" Shekels each.");
 
 	    // Replace this comment with the rest of your code   
 	}
